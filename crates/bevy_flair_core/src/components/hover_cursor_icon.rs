3ce86aae97a45b8f5fd12d1f7712a30b95bf18d0
Totobird <45902300+Totobird-Creations@users.noreply.github.com>
2026-01-14 08:05:50 -0500

    feat: HoveredCursorIcon component which removes CursorIcon on unhover

--- crates/bevy_flair_core/src/components/hover_cursor_icon.rs
+++ crates/bevy_flair_core/src/components/hover_cursor_icon.rs
@@ -6,9 +6,11 @@ use bevy_app::{
 use bevy_ecs::{
     component::Component,
     entity::Entity,
+    lifecycle::HookContext,
     reflect::ReflectComponent,
     query::{ Changed, Or },
-    system::{ Query, Commands }
+    system::{ Query, Commands },
+    world::DeferredWorld
 };
 use bevy_reflect::{
     Reflect,
@@ -22,10 +24,36 @@ use bevy_window::{
 };
 
 
+/// Component which changes the window [`CursorIcon`] when [hovered](Interaction).
 #[derive(Component, Debug, Default, Clone, PartialEq, Eq, Reflect)]
 #[reflect(Component, Debug, Default, Clone, PartialEq)]
+#[component(on_remove = cursor_icon_removed)]
 pub struct HoverCursorIcon {
     pub system : SystemCursorIcon
+}
+
+
+/// Component which tracks which window's [`CursorIcon`] this [`HoverCursorIcon`] is controlling.
+///
+/// Used to remove the component when this entity is unhovered.
+#[derive(Component, Debug, Clone, Copy, PartialEq, Eq, Reflect)]
+#[reflect(Component, Debug, Clone, PartialEq)]
+#[component(immutable)]
+#[component(on_insert = hovered_cursor_icon_inserted)]
+#[component(on_remove = cursor_icon_removed)]
+pub struct HoveredCursorIcon(pub Entity);
+
+fn hovered_cursor_icon_inserted(mut world: DeferredWorld, ctx: HookContext) {
+    let HoveredCursorIcon(window_entity) = *world.get::<HoveredCursorIcon>(ctx.entity).unwrap();
+    let hci = world.get::<HoverCursorIcon>(ctx.entity).unwrap();
+    let icon = CursorIcon::from(hci.system);
+    world.commands().entity(window_entity).insert(icon);
+}
+
+fn cursor_icon_removed(mut world: DeferredWorld, ctx: HookContext) {
+    if let Some(&HoveredCursorIcon(window_entity)) = world.get::<HoveredCursorIcon>(ctx.entity) {
+        world.commands().entity(window_entity).remove::<CursorIcon>();
+    }
 }
 
 
@@ -34,18 +62,23 @@ pub struct HoverCursorPlugin;
 
 impl Plugin for HoverCursorPlugin {
     fn build(&self, app : &mut App) {
-        app.add_systems(Update, update_cursor_icon);
+        app
+            .add_systems(Update, update_cursor_icon);
     }
 }
 
 fn update_cursor_icon(
     mut cmds: Commands,
-    cursor_icons: Query<(&HoverCursorIcon, &Interaction), (Or<(Changed<HoverCursorIcon>, Changed<Interaction>,)>,)>,
+    cursor_icons: Query<(Entity, &HoverCursorIcon, &Interaction), (Or<(Changed<HoverCursorIcon>, Changed<Interaction>,)>,)>,
     windows: Query<(Entity, &Window,),>
 ) {
-    if let Some((icon, _,)) = cursor_icons.iter().find(|(_, interaction,)| matches!(interaction, Interaction::Hovered|Interaction::Pressed)){
-        if let Some((entity, _,)) = windows.iter().find(|(_, window,)| window.focused) {
-            cmds.entity(entity).insert(CursorIcon::from(icon.system));
+    for (interacted_entity, icon, interaction,) in &cursor_icons {
+        if let Interaction::Hovered|Interaction::Pressed = interaction {
+            if let Some((window_entity, _,)) = windows.iter().find(|(_, window,)| window.focused) {
+                cmds.entity(interacted_entity).insert(HoveredCursorIcon(window_entity));
+            }
+        } else {
+            cmds.entity(interacted_entity).remove::<HoveredCursorIcon>();
         }
     }
 }
